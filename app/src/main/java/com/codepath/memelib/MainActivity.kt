a39650ea84c04f294bcc3e0d76408a74cadbb8cf
amey007 <ameynarvekar01@gmail.com>
2022-04-15 18:30:01 -0400

    Code changes for M1 - SignIn/SignUp screen

--- app/src/main/java/com/codepath/memelib/MainActivity.kt
+++ app/src/main/java/com/codepath/memelib/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.codepath.memelib
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.parse.ParseUser
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var logoutbtn: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        logoutbtn = findViewById<Button>(R.id.btnLogout)
+        logoutbtn.isEnabled = ParseUser.getCurrentUser() != null
+
+        logoutbtn.setOnClickListener {
+            //Launch the camera for the user to take picture
+            ParseUser.logOut()
+            goToLoginActivity()
+        }
+    }
+
+    private fun goToLoginActivity() {
+        val intent = Intent(this@MainActivity, LoginActivity::class.java)
+        startActivity(intent)
+        finish()             //closes the MainActivity, avoiding going back to main page on clicking back
     }
 }
