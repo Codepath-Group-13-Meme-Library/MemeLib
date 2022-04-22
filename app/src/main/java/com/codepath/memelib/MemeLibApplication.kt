d82bd6d6cd74aa5fbf13989af2d51fc943734d82
Luna Dagci <ldagci@albany.edu>
2022-04-22 14:38:37 -0400

    Feed page completed

--- app/src/main/java/com/codepath/memelib/MemeLibApplication.kt
+++ app/src/main/java/com/codepath/memelib/MemeLibApplication.kt
@@ -2,11 +2,16 @@ package com.codepath.memelib
 
 import android.app.Application
 import com.parse.Parse
+import com.parse.ParseObject
+
+
+
 
 // Code snippet Implemented from Back4app official documentation
 class MemeLibApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+        ParseObject.registerSubclass(Post::class.java)
         // Initialize Parse on application launch
         Parse.initialize(
             Parse.Configuration.Builder(this)
