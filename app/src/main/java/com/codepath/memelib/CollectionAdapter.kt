390156d728f7c9e9b3adab5997263801d3ebffd2
BrotherOrange <zjh991600@163.com>
2022-04-30 02:11:50 -0400

    milestone3

--- app/src/main/java/com/codepath/memelib/CollectionAdapter.kt
+++ app/src/main/java/com/codepath/memelib/CollectionAdapter.kt
@@ -7,9 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
-import android.widget.TextClock
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.FragmentManager
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.codepath.memelib.fragments.CollectionFragment
